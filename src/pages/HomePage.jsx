8b19c0468d0771a115123b93f5b8b8d7287f58a6
asrisitorus04 <asrisitorus04@gmail.com>
2022-11-04 19:44:42 +0700

    Fixed Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import { ButtonJoin } from 'components/Button';
 import items from "assets/items-bg.svg";
 import React from 'react'
 import { WithRouter } from "utils/Navigation";
+import { BsPeopleFill } from "react-icons/bs";
 
 const HomePage = () => {
   return (
@@ -50,39 +51,26 @@ const HomePage = () => {
           </div>
   
           <div className="flex-auto w-64">
-          <div className="grid grid-cols-4 gap-4">
+          <div className="grid grid-cols-4 gap-4 py-10">
             <div className="">
-              <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16 bg-[#FEF3EB]">
-              <path className="font-primary" strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-              </svg>
+                <p className="text-white flex items-center justify-center font-medium text-sm md:justify-start mb-6">
+                  <BsPeopleFill className="fill-primary text-3xl mr-4" />
+                  230 IS CLIMBERS
+                </p>
             </div>
-            <div>230
-              <div>IS CLIMBERS</div>
-            </div>
-            </div>
-
-            <div>
             <div className="btn-outline">
-              <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-              </svg>
+                <p className="text-white flex items-center justify-center font-medium text-sm md:justify-start mb-6">
+                  <BsPeopleFill className="fill-primary text-3xl mr-4" />
+                  140 
+                  MALE CLIMBERS
+                </p>
             </div>
-            <div>140
-              <div>MALE CLIMBERS</div>
-            </div>
-            </div>
-            </div>
-
-            <div>
             <div className="btn-outline">
-              <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-              </svg>
-            </div>
-            <div>90
-              <div>FEMALE CLIMBER</div>
-            </div>
-            </div>
+                <p className="text-white flex items-center justify-center font-medium text-sm md:justify-start mb-6">
+                  <BsPeopleFill className="fill-primary text-3xl mr-4" />
+                  90 
+                  FEMALE CLIMBERS
+                </p>
             </div>
           </div>
           </div>
