b6ae6a68b0953a3e3e6c9a048bf987d3af906134
radenmasabdul <abdul.tamsis@gmail.com>
2022-11-08 13:30:25 +0700

    Update Minor

--- src/components/ModalAdmin.jsx
+++ src/components/ModalAdmin.jsx
@@ -1,131 +1,334 @@
-import React, { useState, useEffect } from "react";
-import { WithRouter } from "utils/Navigation";
-import { Helmet } from "react-helmet";
-import { apiRequest } from "utils/apiRequest";
+import React, { useState } from "react";
+import { apiRequest } from "../utils/apiRequest";
 
-import { NavbarAdmin, Sidebar } from "components/Navbar";
-import { AiFillDelete } from "react-icons/ai";
+import { InputCustom, InputForModal } from "components/Input";
+import { ButtonCustom } from "components/Button";
 
-import {
-  ModalAdminProduct,
-  ModalEditAdminProduct,
-} from "components/ModalAdmin";
+import { AiFillEdit } from "react-icons/ai";
+import Swal from "sweetalert2";
 
-const AdminProduct = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    apiRequest("admin/product", "get", {})
-      .then((res) => {
-        const results = res.data;
-        setData(results);
-      })
-      .catch((err) => {
-        alert(err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
-  if (loading) {
-    return (
-      <h2
-        id="loading"
-        className="text-secondary font-medium text-lg bg-white text-center mt-80"
-      >
-        Loading Data...
-      </h2>
-    );
-  }
-
+const ModalEditRanger = () => {
   return (
-    <>
-      <Helmet>
-        <title>Admin | GunTour</title>
-        <meta name="description" content="App Description" />
-      </Helmet>
+    <div>
+      {/* The button to open modal */}
+      <label htmlFor="my-modal-6" className="cursor-pointer">
+        <AiFillEdit />
+      </label>
 
-      <div className="mx-auto grid md:grid-flow-col gap-2">
-        <aside className="md:grid-col-span-1">
-          <Sidebar />
-        </aside>
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+      <div className="modal modal-bottom py-5 sm:modal-middle">
+        <div className="bg-white modal-box w-[600px] overflow-auto border p-6">
+          <h3 className="font-bold text-lg text-center">Edit Status Ranger</h3>
+          <hr className="text-line mt-6" />
 
-        <div className="md:grid-col-span-3">
-          <NavbarAdmin />
-          <div className="divider m-0 mr-14" />
+          {/* Form Product */}
+          <div className="mt-6 mb-5">
+            <p className="text-left font-normal text-lg">Status</p>
+            <select
+              // value={objSubmit.category}
+              // onChange={(e) => handleChange(e.target.value, "category")}
+              id="category"
+              class="w-full h-14 border pl-4 rounded-lg text-base mt-2 text-secondary"
+            >
+              <option value="avaliable">Avaliable</option>
+              <option value="duty">Duty</option>
+              <option value="off">Off Work</option>
+            </select>
+          </div>
 
-          <div className="mt-10">
-            <div className="col-span-2">
-              <div className="text-right flex items-center justify-end mt-3 font-medium text-base mr-11">
-                <ModalAdminProduct />
-              </div>
+          <div>
+            <p className="text-left font-normal text-lg">Phone Number</p>
+            <InputCustom
+              // value={objSubmit.phone}
+              // onChange={(e) => handleChange(e.target.value, "phone")}
+              className="w-full h-14 border pl-4 rounded-lg text-base mt-2 text-secondary"
+              placeholder="085797556123"
+            />
+          </div>
 
-              <div className="overflow-x-auto py-6 mr-11">
-                <table className="table w-full">
-                  <thead>
-                    <tr>
-                      <th></th>
-                      <th className="font-semibold text-base text-secondary">
-                        NAME
-                      </th>
-                      <th className="font-semibold text-base text-secondary">
-                        PRICE
-                      </th>
-                      <th className="font-semibold text-base text-secondary">
-                        DESCRIPTION
-                      </th>
-                      <th className="font-semibold text-base text-secondary">
-                        ACTION
-                      </th>
-                    </tr>
-                  </thead>
-                  <>
-                    {data.map((data) => (
-                      <tbody>
-                        <tr>
-                          <th>{data.id_product}</th>
-                          <td>
-                            <div className="grid grid-cols-1 lg:grid-cols-2">
-                              <div className="flex">
-                                <img
-                                  src={data.product_picture}
-                                  alt={data.product_name}
-                                  className="max-w-xl h-14 w-14 rounded-lg"
-                                />
-                                <p className="ml-5 mt-4">{data.product_name}</p>
-                              </div>
-                            </div>
-                          </td>
-                          <td>{data.rent_price}/day</td>
-                          <td>{data.detail}</td>
-                          <td>
-                            <div className="flex items-center text-sm">
-                              <button className="text-2xl text-gray-600">
-                                <ModalEditAdminProduct />
-                              </button>
-                              <button>
-                                <AiFillDelete className="fill-red-600 text-3xl mr-14 ml-4" />
-                              </button>
-                            </div>
-                          </td>
-                        </tr>
-                      </tbody>
-                    ))}
-                  </>
-                </table>
-              </div>
+          {/* Button Add and Close */}
+          <hr className="mt-9" />
+          <div>
+            <ButtonCustom
+              // onClick={() => handleEditProduct()}
+              className="bg-primary mt-6 rounded-lg h-11 w-full flex items-center justify-center font-normal text-sm text-white cursor-pointer"
+              label="Edit Info"
+            />
+
+            <div className="modal-action">
+              <label htmlFor="my-modal-6">
+                <ButtonCustom
+                  className="border h-11 w-[464px] rounded-lg  flex items-center justify-center font-normal text-sm text-[#999999] cursor-pointer"
+                  label="Close"
+                />
+              </label>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default WithRouter(AdminProduct);
+const ModalAdminProduct = () => {
+  const [loading, setLoading] = useState(true);
+  const [objSubmit, setObjSubmit] = useState("");
+
+  const handleAddProduct = async () => {
+    setLoading(true);
+    const formData = new FormData();
+    for (const key in objSubmit) {
+      formData.append(key, objSubmit[key]);
+    }
+    apiRequest("admin/product", "post", objSubmit, "multipart/form-data")
+      .then((res) => {
+        Swal.fire({
+          icon: "success",
+          title: "Data added successfully",
+        });
+        setObjSubmit({});
+      })
+      .catch((err) => {
+        Swal.fire({
+          icon: "warning",
+          title: "There is an error please check again",
+        });
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const handleChange = (value, key) => {
+    let temp = { ...objSubmit };
+    temp[key] = value;
+    setObjSubmit(temp);
+  };
+
+  return (
+    <div>
+      <label
+        htmlFor="my-modal-3"
+        className="cursor-pointer font-normal text-base text-center justify-center h-12 w-[180px] rounded-lg text-white bg-primary transform active:scale-95 transition-transform flex items-center"
+      >
+        Add Product
+      </label>
+
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+
+      <div className="modal">
+        <div className="modal-box rounded-lg relative">
+          <h3 className="font-semibold text-xl text-secondary text-center">
+            Data Product
+          </h3>
+          <div className="divider m-0" />
+
+          {/* Add Photo Product */}
+          <div className="mt-6 mb-5">
+            <h3 className="font-normal text-secondary text-lg text-left mb-3">
+              Photo Product
+            </h3>
+            <div className="text-left">
+              <input
+                value={objSubmit.product_picture}
+                onChange={(e) =>
+                  handleChange(e.target.value, "product_picture")
+                }
+                type="file"
+                className="file-input w-full max-w-xs"
+              />
+            </div>
+          </div>
+
+          {/* Input Product Name*/}
+          <div className="mb-5">
+            <p className="font-normal text-lg text-secondary text-left mb-3">
+              Product Name
+            </p>
+            <InputForModal
+              value={objSubmit.product_name}
+              onChange={(e) => handleChange(e.target.value, "product_name")}
+              placeholder="Enter your product name"
+            />
+          </div>
+
+          {/* Input Rent Price*/}
+          <div className="mb-5">
+            <p className="font-normal text-lg text-secondary text-left mb-3">
+              Rent Price
+            </p>
+            <InputForModal
+              value={objSubmit.rent_price}
+              onChange={(e) => handleChange(e.target.value, "rent_price")}
+              placeholder="100K"
+            />
+          </div>
+
+          {/* Input Description*/}
+          <div className="mb-5">
+            <p className="font-normal text-lg text-secondary text-left mb-3">
+              Description
+            </p>
+            <textarea
+              value={objSubmit.detail}
+              onChange={(e) => handleChange(e.target.value, "detail")}
+              className="w-full border border-[#B3B3B3] textarea mt-2 rounded-lg h-40 text-base font-normal"
+              placeholder="Enter your product description"
+            ></textarea>
+          </div>
+
+          {/* Input Note Warning*/}
+          <div className="mb-5">
+            <p className="font-normal text-lg text-secondary text-left mb-3">
+              Warning
+            </p>
+            <textarea
+              value={objSubmit.note}
+              onChange={(e) => handleChange(e.target.value, "note")}
+              className="w-full border border-[#B3B3B3] textarea mt-2 rounded-lg h-40 text-base font-normal"
+              placeholder="Enter your product description"
+            ></textarea>
+          </div>
+
+          <div className="divider m-0" />
+          <ButtonCustom
+            onClick={() => handleAddProduct()}
+            className="cursor-pointer font-medium text-center justify-center h-11 w-full mt-5 mb-3 rounded-lg text-white bg-primary transform active:scale-95 transition-transform flex items-center"
+            label="Save Data"
+          />
+
+          <label htmlFor="my-modal-3">
+            <ButtonCustom
+              className="cursor-pointer font-medium text-center justify-center h-11 w-full rounded-lg text-slate-400 border bg-white transform active:scale-95 transition-transform flex items-center"
+              label="Close"
+            />
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ModalEditAdminProduct = () => {
+  return (
+    <div>
+      <label htmlFor="my-modal-3" className="text-2xl text-gray-600">
+        <AiFillEdit />
+      </label>
+
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+
+      <div className="modal">
+        <div className="modal-box rounded-lg relative">
+          <h3 className="font-semibold text-xl text-secondary text-center">
+            Edit Product
+          </h3>
+          <div className="divider m-0" />
+
+          {/* Add Photo Product */}
+          <div className="mt-6 mb-5">
+            <h3 className="font-normal text-secondary text-lg text-left mb-3">
+              Photo Product
+            </h3>
+            <div className="text-left">
+              <input type="file" className="file-input w-full max-w-xs" />
+            </div>
+          </div>
+
+          {/* Input Product Name*/}
+          <div className="mb-5">
+            <p className="font-normal text-lg text-secondary text-left mb-3">
+              Product Name
+            </p>
+            <InputForModal placeholder="Enter your product name" />
+          </div>
+
+          {/* Input Rent Price*/}
+          <div className="mb-5">
+            <p className="font-normal text-lg text-secondary text-left mb-3">
+              Rent Price
+            </p>
+            <InputForModal placeholder="100K" />
+          </div>
+
+          {/* Input Description*/}
+          <div className="mb-5">
+            <p className="font-normal text-lg text-secondary text-left mb-3">
+              Description
+            </p>
+            <textarea
+              className="w-full border border-[#B3B3B3] textarea mt-2 rounded-lg h-40 text-base font-normal"
+              placeholder="Enter your product description"
+            ></textarea>
+          </div>
+
+          <div className="divider m-0" />
+          <ButtonCustom
+            className="cursor-pointer font-medium text-center justify-center h-11 w-full mt-5 mb-3 rounded-lg text-white bg-primary transform active:scale-95 transition-transform flex items-center"
+            label="Edit Product"
+          />
+
+          <label htmlFor="my-modal-3">
+            <ButtonCustom
+              className="cursor-pointer font-medium text-center justify-center h-11 w-full rounded-lg text-slate-400 border bg-white transform active:scale-95 transition-transform flex items-center"
+              label="Close"
+            />
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ModalAdminUSer = () => {
+  return (
+    <div>
+      <label
+        htmlFor="my-modal-3"
+        className="cursor-pointer font-medium text-lg text-center justify-center h-12 w-[183px] rounded-lg px-6 py-1 text-white bg-[#E5640E] transform active:scale-75 transition-transform flex items-center hover:bg-primary hover:text-black"
+      >
+        Edit Data
+      </label>
+
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+          <h3 className="font-semibold text-xl text-secondary text-center">
+            Edit Climbers Data
+          </h3>
+          <div className="divider text-[#E2E7E9]" />
+          <p className="font-normal text-lg text[#373737] text-left">
+            Is Climbers
+          </p>
+          <InputForModal />
+          <p className="font-normal text-lg text[#373737] text-left">
+            Male Climbers
+          </p>
+          <InputForModal />
+          <p className="font-normal text-lg text[#373737] text-left">
+            Female Climbers
+          </p>
+          <InputForModal />
+          <div className="divider text-[#E2E7E9]" />
+          <ButtonCustom
+            className="cursor-pointer font-medium text-center justify-center h-11 w-full px-6 py-1 rounded-lg text-white bg-primary transform active:scale-75 transition-transform flex items-center hover:bg-primary hover:text-white"
+            label="Save"
+          />
+          <br />
+          <label htmlFor="my-modal-3">
+            <ButtonCustom
+              className="cursor-pointer font-medium text-center justify-center h-11 w-full px-6 py-1 rounded-lg text-[#999999] bg-white transform active:scale-75 transition-transform flex items-center hover:bg-primary hover:text-white"
+              label="Close"
+            />
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export {
+  ModalEditRanger,
+  ModalAdminProduct,
+  ModalEditAdminProduct,
+  ModalAdminUSer,
+};
