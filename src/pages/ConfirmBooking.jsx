a8da7e20366b8b8b0640270de7ce3e7d44b7f700
radenmasabdul <abdul.tamsis@gmail.com>
2022-11-08 10:58:18 +0700

    Responsive Page Confirm

--- src/pages/ConfirmBooking.jsx
+++ src/pages/ConfirmBooking.jsx
@@ -7,34 +7,31 @@ import { ButtonCancelBooking, ButtonConfirmBooking } from "components/Button";
 const ConfirmBooking = () => {
   return (
     <Layout>
-      <section className="mx-24 mt-14 mb-64">
-        <div className="bg-white py-7 px-8 rounded-lg">
-          <h5 className="font-semibold text-xl text-secondary mb-6">
+      <main className="grid grid-cols-1 mx-20 py-20">
+        <section className="grid grid-flow-row">
+          <h5 className="font-semibold text-xl text-secondary">
             Payment Description
           </h5>
-          <div className="divider m-0" />
-          <div className="my-6">
-            <p className="font-medium text-lg text-secondary mb-5">
-              Ticket Quantity: 2
-            </p>
-            <p className="font-medium text-lg text-secondary mb-5">
-              Items Booking: Camping Tent, Mat
-            </p>
-            <p className="font-medium text-lg text-secondary mb-5">
-              Rangers: Fajar Nugraha
-            </p>
-            <p className="font-medium text-lg text-secondary">
-              Total Price: Rp 198K
-            </p>
+          <div className="divider" />
+          <p className="font-medium text-lg text-secondary">
+            Ticket Quantity: 2
+          </p>
+          <p className="font-medium text-lg text-secondary py-2">
+            Items Booking: Camping Tent, Mat
+          </p>
+          <p className="font-medium text-lg text-secondary pb-2">
+            Rangers: Fajar Nugraha
+          </p>
+          <p className="font-medium text-lg text-secondary">
+            Total Price: Rp 198K
+          </p>
+          <div className="divider" />
+          <ButtonConfirmBooking />
+          <div className="pt-2">
+            <ButtonCancelBooking />
           </div>
-          <div className="divider m-0" />
-          <div className="mt-12 mb-6">
-            <ButtonConfirmBooking />
-          </div>
-
-          <ButtonCancelBooking />
-        </div>
-      </section>
+        </section>
+      </main>
     </Layout>
   );
 };
