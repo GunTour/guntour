fe19ad0adf9df17b1e9be669a9a331c4ea1cfbf8
radenmasabdul <abdul.tamsis@gmail.com>
2022-11-09 19:08:53 +0700

    Update Booking

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -130,12 +130,14 @@ const CardOrderBooking = (props) => {
         <h2 className="card-title font-semibold text-2xl text-secondary">
           Your Booking #{props.idBooking}
         </h2>
-        <p className="font-medium text-xl text-secondary">Rp. {props.priceBooking}</p>
+        <p className="font-medium text-xl text-secondary">
+          Rp. {props.priceBooking}
+        </p>
         <p className="py-1 px-2 w-32 h-9 rounded-lg bg-[#FFF1CC] text-primary text-center font-medium text-base">
           {props.statusBooking}
         </p>
         <div className="card-actions justify-center">
-          <a href={props.payBooking}> 
+          <a href={props.payBooking}>
             <button className="login font-medium text-center justify-center h-[48px] w-[265px] px-6 py-1 rounded-md text-white bg-primary transform active:scale-75 transition-transform flex items-center hover:bg-primary hover:text-white ">
               Pay Now
             </button>
