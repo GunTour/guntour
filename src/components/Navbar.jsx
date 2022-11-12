46e1bf3a7f6d4cd57e242b654f2aa9f21dd2c4f2
asrisitorus04 <asrisitorus04@gmail.com>
2022-11-12 20:24:34 +0700

    update ranger page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -177,30 +177,6 @@ const NavbarAdmin = () => {
           <label htmlFor="my-drawer-2" className="normal-case font-semibold text-2xl">Dashboard</label>
         </div>
         <div className="flex-none gap-2">
-          <div className="w-3/6 h-10 md:flex flex-none mt-form-control mr-16 input hidden bg-[#F2F2F2] border items-center">
-            <input
-              type="text"
-              placeholder="Search"
-              className="w-full outline-none text-[#cccccc] bg-[#F2F2F2]"
-            />
-            <button className="py-3.5 border-y-2 border-l-2 border-none bg-main rounded-l-lg ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-[#cccccc]"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                />
-              </svg>
-            </button>
-          </div>
-
           <div className="w-40 pr-10 grid grid-flow-col auto-cols-max">
             <label tabIndex={0} className="btn-circle avatar py-1">
               <div className="w-10 rounded-full">
